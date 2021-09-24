f330d4416c568a2226b5883f2bb6b2cd0e91cbba
tonybwatson <tonybwatson@gmail.com>
2021-09-24 08:46:39 -0400

    working

---
+++ js/main.js
@@ -0,0 +1,76 @@
+// create variables for all pages
+let pageOne = document.getElementById('pageOne');
+let pageTwo = document.getElementById('pageTwo');
+let pageThree = document.getElementById('pageThree');
+let pageFour = document.getElementById('pageFour');
+let pageFive = document.getElementById('pageFive');
+let pageSix = document.getElementById('pageSix');
+
+// create array of pages
+var pagesArr = [
+    pageOne,
+    pageTwo,
+    pageThree, 
+    pageFour,
+    pageFive,
+    pageSix
+];
+
+// create string for randomString
+var symbolString = '!@#$%^&*()';
+
+// variable for first and last items in array
+var firstItem = pagesArr[0];
+var lastItem = pagesArr[pagesArr.length - 1];
+
+// function to make pages visible
+function pageSwitch() {
+    // move first page to end of array, second page to front of array
+    var firstToLast = pagesArr.push(pagesArr[0]);
+    var lastFirst = pagesArr.shift();
+    // console.log(pagesArr);
+
+    // make first page in array visible, last page invisible
+    var firstItem = pagesArr[0];
+        firstItem.style.display = "block";
+    var lastItem = pagesArr[pagesArr.length - 1];
+        lastItem.style.display = "none";
+}
+
+// generate random symbol from symbolString
+function randomSymbol() {
+    // variable to grab letter from symbolString by running Math.random rounded down multiplied by length of symbolString
+   var randSymbol = symbolString[Math.floor(Math.random() * symbolString.length)];
+   return randSymbol;
+} 
+console.log(randomSymbol())
+var yourSymbol = randomSymbol();
+console.log(yourSymbol)
+
+// array to hold symbols to display
+var symbolArr = [];
+
+// loop through numbers 1-99, push to symbolArr
+for (let i = 0; i <= 99; i++) {
+    var currentString = "";
+    if (i % 9 === 0) {
+        currentString = i + ": " + yourSymbol;
+    } else {
+        currentString = i + ": " + randomSymbol();
+    }
+    symbolArr.push('<br>' + currentString);
+    } 
+    symbolString = symbolArr.join(',', '')
+    console.log(symbolArr)
+
+    // set symbol to match to multiples of 9
+function setYourSymbol() {
+    document.getElementById('yourSymbol').innerHTML = yourSymbol;
+}
+
+setYourSymbol();
+
+// set random symbols in array
+function setSymbols() {
+    document.getElementById('symbolArr').innerHTML = symbolArr;
+}
---
+++ pseudocode.md
@@ -0,0 +1,53 @@
+Upon clicking a button, player moves to a different page.
+
+
+Various page states in an array. 
+Switch between pages when button clicked.
+Reset button brings player back to start, resets randomSymbols()
+
+Objects
+    nextButton -  onclick, display next page in array and hide current
+    restartButton - onclick, goes back to beginning of pagesArr
+    page - holds information for what should be on page (several different ones)
+    symbolString - string of symbols
+    yourSymbol - specific symbol chosen for multiples of 9
+
+
+Arrays
+    pagesArr - holds page states
+    symbolsArr - displays randomised symbols matched to numbers, multiples of 9 are yourSymbol
+
+Variables
+    
+
+Functions
+    pageSwitch() - display pagesArr(this page + 1)
+    backToStart() - display pagesArr[0]
+    randomSymbol() - chooses a random symbol from string of symbols and places at front of symbolsArr
+
+
+START
+
+INIT ()
+    button
+    restartButton
+    page
+    pages[]
+
+Show first item in pagesArr
+randomSymbol()
+IF nextButton clicked, display next page in pagesArr and hide current page
+IF restartButton clicked, go to pagesArr[0]
+generate randomSymbol()
+(repeats until page 4)
+store randomSymbol
+???
+Profit
+display symbolsArr
+on page6, show randomSymbol; message "Your symbol is (randomSymbol)"; and restartButton
+
+
+create mostly random array of numbers & symbols
+for numbers between 1 & 99
+    add number to array as (number: (randomSymbol))
+IF array item is multiple of 9, replace with yourSymbol
